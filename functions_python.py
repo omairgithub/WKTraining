1d02f18c34c3f434f09e927d9f106496beb25459
omair <shaikhomair1996@gmail.com>
2022-06-28 14:42:52 +0530

    function

---
+++ functions_python.py
@@ -0,0 +1,9 @@
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome")
+
+def greet1(name):
+    return f"hi {name}"
+
+greet("omair", "shaikh")
+print(greet1("omair"))
---
+++ varArgsKeywords.py
@@ -0,0 +1,4 @@
+def add_user(**user):
+    print(user)
+
+add_user(id=1,name="omair",age=26)
